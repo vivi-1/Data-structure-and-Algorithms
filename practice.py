b15e9d55f43f622063d15204143956eb52acadfe
vivi-1 <wwei6vt.edu>
2021-02-13 21:20:45 -0500

    Chapter3

---
+++ practice.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+#Chapter 3
+"""
+Q1: function​ isLeapYear(year) {
+​ 	  ​return​ (year % 100 === 0) ? (year % 400 === 0) : (year % 4 === 0);
+    }
+
+O(1)
+
+Q2: ​function​ arraySum(array) {
+​ 	  ​let​ sum = 0;
+​
+​ 	  ​for​(​let​ i = 0; i < array.length; i++) {
+​ 	    sum += array[i];
+​ 	  }
+​
+​ 	  ​return​ sum;
+​ 	}
+O(N)
+
+Q3: O(logN)
+Q4: O(N)
+Q5: O(1)
+
+"""
