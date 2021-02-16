62d58f3758e3a18159362fa48cbe2a734d688be8
vivi-1 <wwei6@vt.edu>
2021-02-15 22:01:12 -0500

    Chapter 10 end

---
+++ Chapter10.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+#Chapter 10 Q4
+def print_mix(arr):
+    for i in arr:
+        if isinstance(i, int) == True:
+            print (i)
+        else:
+            print_mix(i)
+array = [1,2,3,[4, 5, 6], 7,[8,[9, 10, 11,[12, 13, 14]]],[15, 16, 17, 18, 19,[20, 21, 22,[23, 24, 25,[26, 27, 29]], 30, 31], 32], 33]
+print_mix(array)
